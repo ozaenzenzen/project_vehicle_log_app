62c30edc683869b25c679e376f8e8f3d636692e6
Fauzan Akmal Mahdi <recovery252@gmail.com>
2024-10-22 19:20:55 +0700

    update init config

--- lib/init_config.dart
+++ lib/init_config.dart
@@ -13,7 +13,7 @@ class AppInitConfig {
     AppLocalStorage.init();
     // EnvironmentConfig.customBaseUrl = "https://4be5-112-215-170-211.ngrok.io"; // for ngrok
 
-    EnvironmentConfig.customBaseUrl = "https://2e99-114-10-42-123.ngrok-free.app"; // for ngrok
+    EnvironmentConfig.customBaseUrl = "https://8230-114-10-42-123.ngrok-free.app"; // for ngrok
 
     // EnvironmentConfig.customBaseUrl = "http://10.0.2.2:8080"; // for emulator android
     // EnvironmentConfig.customBaseUrl = "http://localhost:8080"; // for emulator iOS
