a9fea3180fcd4f45a16d059587e3574642c366fd
Fauzan Akmal Mahdi <recovery252@gmail.com>
2024-10-30 19:36:50 +0700

    update signout bloc

--- lib/presentation/profile_screen/profile_page.dart
+++ lib/presentation/profile_screen/profile_page.dart
@@ -5,8 +5,6 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:project_vehicle_log_app/data/dummy_data_profile.dart';
-import 'package:project_vehicle_log_app/data/local_repository/account_local_repository.dart';
-import 'package:project_vehicle_log_app/data/local_repository/vehicle_local_repository.dart';
 import 'package:project_vehicle_log_app/presentation/profile_screen/profile_bloc/profile_bloc.dart';
 import 'package:project_vehicle_log_app/presentation/profile_screen/signout_bloc/signout_bloc.dart';
 import 'package:project_vehicle_log_app/presentation/signin_screen/signin_page.dart';
