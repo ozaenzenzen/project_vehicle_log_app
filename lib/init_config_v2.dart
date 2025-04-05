010129e710088582c2de854c34c38803f22f7d87
Fauzan Akmal Mahdi <recovery252@gmail.com>
2025-04-05 14:54:59 +0700

    update url

--- lib/init_config_v2.dart
+++ lib/init_config_v2.dart
@@ -46,7 +46,7 @@ class AppInitConfig {
     await famCodingSupply.appDeviceInfo.getDeviceData();
 
     // EnvironmentConfig.customBaseUrl = "https://4be5-112-215-170-211.ngrok.io"; // for ngrok
-    EnvironmentConfig.customBaseUrl = "https://f1fd-114-10-42-224.ngrok-free.app"; // for ngrok
+    EnvironmentConfig.customBaseUrl = "https://5dbb-114-10-42-38.ngrok-free.app"; // for ngrok
     // EnvironmentConfig.customBaseUrl = "http://10.0.2.2:8080"; // for emulator android
     // EnvironmentConfig.customBaseUrl = "http://localhost:8080"; // for emulator iOS
 
